a81bcde9fb57c256064588d4efd20f965478d0cf
NareshKeth <60884023+NareshKeth@users.noreply.github.com>
2020-03-10 20:00:57 +0530

    Update App.java
    App.java Edited

--- maven/src/main/java/com/keth/maven/App.java
+++ maven/src/main/java/com/keth/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Welcomes you..--***####" );
+        System.out.println( "Hello World! Welcomes you..--***####***" );
     }
 }
